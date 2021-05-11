b9b67f6a5afcadfe84d934c0e0dd366273a1e3ae
RicardoMartinez <ricardoemartine@hotmail.com>
2021-05-11 22:28:59 +0200

    start shopify custom design with react

---
+++ src/context/ShopContext.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Provider } from 'styletron-react';
+
+class Shop Provider extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Shop Provider;
